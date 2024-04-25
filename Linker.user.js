2a2ff9610f6a4241e448dc964b8eea41644c2a79
ForKorny <81922393+ForKorny@users.noreply.github.com>
2024-04-25 12:31:59 +0300

    Update Linker.user.js

--- Linker.user.js
+++ Linker.user.js
@@ -3,15 +3,25 @@
 // @version      1.4
 // @description  quick link selectors
 // @author       Arekusei
-// @include      *://pixelplanet.fun/#*
-// @include      *://fuckyouarkeros.fun/#*
-// @updateURL    https://github.com/Arsenicus/Linker/raw/main/Linker.user.js
-// @downloadURL  https://github.com/Arsenicus/Linker/raw/main/Linker.user.js
+// @match      *://pixelplanet.fun/*
+// @match      *://fuckyouarkeros.fun/*
+// @match      *://pixmap.fun/*
+// @match      *://pixuniverse.fun/*
+// @match      *://pixelya.fun/*
+// @match      *://globalpixel.xyz/*
+// @match      *://planetcanvas.online/*
+// @match      *://pxgame.xyz/*
+// @match      *://pixarea.fun/*
+// @match      *://pixland.fun/*
+// @match      *://canvaspixel.net/*
+// @updateURL    https://github.com/ForKorny/Linker2024/raw/main/Linker.user.js
+// @downloadURL  https://github.com/ForKorny/Linker2024/raw/main/Linker.user.js
 
 // ==/UserScript==
 
 !function() {
   "use strict";
   var e = document.createElement("script");
-  e.src = "https://arsenicus.github.io/Linker/Linker.js", document.body.appendChild(e)
+  e.src = "https://forkorny.github.io/Linker2024/Linker.js";
+  document.body.appendChild(e);
 }();
